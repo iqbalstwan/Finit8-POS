89cec58661c2bef0b5464e2432ea684faf64afe8
iqbalstwangit config ==global user.email iqbalstwan@yahoo.comgit config --global user.name iqbalstwan <iqbalstwan@yahoo.com>
2020-09-12 23:09:03 +0700

    Add Setting Category

---
+++ src/views/Setting.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <HistHeader text="Category" />
+    <HistContent />
+  </div>
+</template>
+
+<script>
+import HistHeader from '../components/_base/HistHeader'
+import HistContent from '../components/_base/HistContent'
+
+export default {
+  name: 'Setting',
+  components: {
+    HistHeader,
+    HistContent
+    // Histcontent
+  }
+}
+</script>
